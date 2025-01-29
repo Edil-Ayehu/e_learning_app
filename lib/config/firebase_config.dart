1db4de94ac45918dc4b53a16ae3ebffd7eba80a8
Edilayehu Tadesse <edilayehu534027@gmail.com>
2025-01-29 00:33:00 -0800

    firebase integration

--- lib/config/firebase_config.dart
+++ lib/config/firebase_config.dart
@@ -1,7 +1,10 @@
+import 'package:e_learning_app/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 class FirebaseConfig {
   static Future<void> init() async {
-    await Firebase.initializeApp();
+      await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   }
 }
