550f5dd1e1aee9e237f942548e144a7498d5571d
Edilayehu Tadesse <edilayehu534027@gmail.com>
2025-02-18 04:26:05 -0800

    notification refactoring

---
+++ lib/views/notifications/widgets/notifications_list.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:e_learning_app/views/notifications/widgets/notification_card.dart';
+
+class NotificationsList extends StatelessWidget {
+  const NotificationsList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      itemCount: 5,
+      itemBuilder: (context, index) {
+        return NotificationCard(
+          title: 'New Course Available',
+          message: 'Check out our new Flutter Advanced course!',
+          time: '2 hours ago',
+          icon: Icons.school,
+          isUnread: index == 0,
+        );
+      },
+    );
+  }
+}
