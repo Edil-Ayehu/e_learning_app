352956737f3ab436fb68f43d4fd9fc3be9136658
Edilayehu Tadesse <edilayehu534027@gmail.com>
2025-03-05 03:26:54 -0800

    teacher home screen refactoring

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -24,7 +24,7 @@ import 'package:e_learning_app/views/teacher/create_course_screen.dart';
 import 'package:e_learning_app/views/teacher/my_courses_screen.dart';
 import 'package:e_learning_app/views/teacher/student_progress_screen.dart';
 import 'package:e_learning_app/views/teacher/teacher_analytics_screen.dart';
-import 'package:e_learning_app/views/teacher/teacher_home_screen.dart';
+import 'package:e_learning_app/views/teacher/teacher_home_screen/teacher_home_screen.dart';
 import 'package:flutter/material.dart';
 
 class AppRoutes {
