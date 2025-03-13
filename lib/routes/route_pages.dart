a42ea1d00eae57372ad411c04bbc43618b2bfdeb
Edilayehu Tadesse <edilayehu534027@gmail.com>
2025-03-13 04:15:03 -0700

    analytics screen

--- lib/routes/route_pages.dart
+++ lib/routes/route_pages.dart
@@ -27,7 +27,7 @@ import 'package:e_learning_app/views/splash/splash_screen.dart';
 import 'package:e_learning_app/views/teacher/create_course/create_course_screen.dart';
 import 'package:e_learning_app/views/teacher/my_courses/my_courses_screen.dart';
 import 'package:e_learning_app/views/teacher/student_progress/student_progress_screen.dart';
-import 'package:e_learning_app/views/teacher/teacher_analytics_screen.dart';
+import 'package:e_learning_app/views/teacher/teacher_analytics/teacher_analytics_screen.dart';
 import 'package:e_learning_app/views/teacher/teacher_home_screen/teacher_home_screen.dart';
 import 'package:get/get.dart';
 
